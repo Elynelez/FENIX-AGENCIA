4479ed2692085fc5e198d45a6c6d20208c1491b3
Elynelez <johndoe@example.comElinarnyex@gmail.com>
2024-06-15 17:22:10 -0500

    buscador y corrección diseño

--- fenixagencia/app/components/Navbar/Navbar.tsx
+++ fenixagencia/app/components/Navbar/Navbar.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+"use client";
+
+import React, { useState, FormEvent } from 'react';
 import { IoMdSearch, IoIosHeart, IoLogoWhatsapp } from 'react-icons/io';
 import { FaCaretDown } from "react-icons/fa";
+import { useRouter } from 'next/navigation';
 import Darkmode from './Darkmode';
 
 const MenuLinks = [
@@ -18,48 +21,68 @@ const MenuLinks = [
     id: 3,
     name: "Sobre nosotros",
     link: "/about"
-  },
-  // {
-  //   id: 4,
-  //   name: "Solicitud especial",
-  //   link: "/special"
-  // }
+  }
 ]
 
 const DropdownLinks = [
   {
     id: 1,
-    name: "Servicios Tranding",
+    name: "Masajes",
     link: "/#"
   },
   {
     id: 2,
     name: "Sexcam",
     link: "/"
+  },
+  {
+    id: 3,
+    name: "Sexo anal",
+    link: "/"
+  },
+  {
+    id: 3,
+    name: "Oral al natural",
+    link: "/"
+  },
+  {
+    id: 3,
+    name: "Fetiches",
+    link: "/"
   }
 ]
 
 const Navbar = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const router = useRouter();
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    if (searchTerm.trim()) {
+      router.push(`/search/${searchTerm}`);
+    }
+  };
+
   return (
     <div className="dark:bg-gray-900 dark:text-white duration-200 relative z-40">
       <div className='PY-4'>
         <div className="container flex justify-between items-center">
           <div className='flex items-center gap-4'>
             <div className='dark:text-white stext-black font-semibold tracking-widest text 2xl uppercase sm:text-3xl'>
-              <a href="#" >
-                INDUCOR FENIX
-              </a>
-              <a href="" className="flex items-center gap-2 text-base sm:text-sm">
-                <IoLogoWhatsapp className="w-4 h-4"/>
-                <span>3117892346</span>
-              </a>
+              INDUCOR FENIX
             </div>
             <div className='hidden lg:block'>
               <ul className='flex items-center gap-4'>
+                <li>
+                  <a href={"https://api.whatsapp.com/send?phone=573207474085&text=" + "hola sapa perra"} target="_blank" className="flex items-center gap-2 text-gray-500 hover:text-violet-400 font-semibold sm:text-sm">
+                    <IoLogoWhatsapp className="w-4 h-4" />
+                    <span>3117892346</span>
+                  </a>
+                </li>
                 {
                   MenuLinks.map((data, index) => (
                     <li key={index}>
-                      <a href={data.link} className='inline-block px-4 font-semibold text-gray-500 hover:text-black dark:hover:text-white duration-200'
+                      <a href={data.link} className='inline-block px-4 font-semibold text-gray-500 hover:text-violet-400 dark:hover:text-white duration-200'
                       >
                         {data.name}
                       </a>
@@ -67,15 +90,15 @@ const Navbar = () => {
                   ))
                 }
                 <li className='relative cursor-pointer group flex items-center gap-2'>
-                  <a href="$" className='flex items-center gap-[px] font-semibold text-gray-500 dark:hover:text-white py-2'>
+                  <a href="$" className='flex items-center gap-[px] font-semibold text-gray-500 hover:text-violet-400 py-2'>
                     Servicios
                   </a>
-                  <div className='absolute z-[9999] hidden group-hover:block w-[200px] rounded-md bg-white shadow-md dark:bg-gray-900 p-2 dark:text-qhite '>
+                  <div className='absolute top-[40px] hidden group-hover:block w-[200px] rounded-md bg-white shadow-md dark:bg-gray-900 p-2 dark:text-white '>
                     <ul className='space-y-2'>
                       {
                         DropdownLinks.map((data, index) => (
                           <li key={index}>
-                            <a href={data.link} className='text-gray-500 dark:hover:text-white duration-200 inline-block  w-full p-2 hover:bg-primary/20 rpunded-md font-semibold'>
+                            <a href={data.link} className='text-gray-500 dark:hover:text-white duration-200 inline-block w-full p-2 hover:bg-primary/20 rpunded-md font-semibold'>
                               {data.name}
                             </a>
                           </li>
@@ -92,8 +115,16 @@ const Navbar = () => {
           </div>
           <div className='flex justify-between items-center gap-4'>
             <div className='relative group hidden sm:block'>
-              <input type="text" placeholder='search' className='search-bar' />
-              <IoMdSearch className='text-xl text-gray-600 group-hover:text-pretty dark:text-gray-400 absolute top-1/2 -translate-y-1/2 right-1 duration-200' />
+              <form onSubmit={handleSubmit}>
+                <input
+                  type="text"
+                  placeholder='buscar'
+                  className='search-bar'
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <IoMdSearch className='text-xl text-gray-600 group-hover:text-pretty dark:text-gray-400 absolute top-1/2 -translate-y-1/2 right-1 duration-200' />
+              </form>
             </div>
             <button className='relative p-3'>
               <IoIosHeart className='text-xl text-gray-600 dark:text-gray-400' />
---
+++ fenixagencia/app/search/[query]/page.tsx
@@ -0,0 +1,37 @@
+// app/search/[query]/page.tsx
+
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import girls from '../../girls.json'
+
+const SearchPage = ({ params }: { params: { query: string } }) => {
+  const query = params.query;
+
+  return (
+    <div>
+      <h1>Resultados para "{query}"</h1>
+      <div className="min-h-screen flex flex-col">
+        <main className="flex-grow p-4">
+          <div className="text-gray-600 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            {girls.filter(girl => girl.name.includes(query)).map(girl => (
+              <div key={girl.id} className="group-image p-4 rounded-lg ">
+                <a href={`/girl/${girl.id}`}>
+                  <figure className="mb-4">
+                    <img src={girl.photo[0]} alt={girl.name} className="w-full h-auto rounded-lg w-[300px] sm:w-[450px] h-[200px] sm:h-[250px]" />
+                  </figure>
+                </a>
+                <div>
+                  <h2 className="text-xl font-bold mb-2 ">{girl.name}</h2>
+                  <p className="text-lg mb-2">{girl.price}</p>
+                  <p className="text-sm">{girl.services.join(", ")}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default SearchPage;
